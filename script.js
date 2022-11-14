a8f41e4a3cef09c45bb26fcf282c10e485a17bfb
Okonkwo-Udoye Chiamaka <okonkwosmmac@gmail.com>
2022-11-14 06:52:08 +0300

    changes

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
-const delay = 3000; //ms
-
-const slides = document.querySelector(".slides");
-const slidesCount = slides.childElementCount;
-const maxLeft = (slidesCount - 1) * 100 * -1;
-
-let current = 0;
-
-function changeSlide(next = true) {
-  if (next) {
-    current += current > maxLeft ? -100 : current * -1;
-  } else {
-    current = current < 0 ? current + 100 : maxLeft;
-  }
-
-  slides.style.left = current + "%";
+indexValue = 1;
+showImg(indexValue);
+function side_slide(e) {
+	showImg(indexValue += e);
 }
-
-let autoChange = setInterval(changeSlide, delay);
-const restart = function() {
-  clearInterval(autoChange);
-  autoChange = setInterval(changeSlide, delay);
-};
-
-// Controls
-document.querySelector(".next-slide").addEventListener("click", function() {
-  changeSlide();
-  restart();
-});
-
-document.querySelector(".prev-slide").addEventListener("click", function() {
-  changeSlide(false);
-  restart();
-});
+function showImg(e) {
+	var i;
+	const img = document.querySelectorAll('img')
+	if (e > img.length) {
+		indexValue = 1;
+	}
+	if (e < 1) {
+		indexValue = img.length;
+	}
+	for (i = 0; i < img.length; i++) {
+		img[i].style.display = 'none';
+	}
+	img[indexValue - 1].style.display = 'block';
+}
